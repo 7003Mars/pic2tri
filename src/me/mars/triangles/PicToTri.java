7e94c8d04d337d142d2857f25faded3895bc4b18
Mars <>
2023-04-09 18:22:56 +0800

    Default schematics for first mod run

--- src/me/mars/triangles/PicToTri.java
+++ src/me/mars/triangles/PicToTri.java
@@ -4,18 +4,46 @@ import arc.Core;
 import arc.Events;
 import arc.scene.ui.TextField;
 import arc.scene.ui.layout.Table;
+import arc.struct.Seq;
+import arc.struct.StringMap;
 import arc.util.Log;
 import arc.util.OS;
 import arc.util.Strings;
 import me.mars.triangles.ui.ConverterDialog;
 import mindustry.Vars;
+import mindustry.content.Blocks;
 import mindustry.game.EventType;
+import mindustry.game.Schematic;
 import mindustry.mod.Mod;
 import mindustry.ui.dialogs.BaseDialog;
 import mindustry.ui.dialogs.SettingsMenuDialog;
+import mindustry.world.Block;
+import mindustry.world.Tile;
+import mindustry.world.blocks.logic.LogicBlock;
+
 import static mindustry.ui.dialogs.SettingsMenuDialog.SettingsTable.TextSetting;
 
 public class PicToTri extends Mod {
+	private static final String reloadScript = """
+			print "For use with the PicToTri mod. Link to all the displays you wish to load then click the switch"
+			sensor e switch1 @enabled
+			jump 1 notEqual e 1
+			set i 0
+			getlink b i
+			sensor type b @type
+			jump 10 equal type @logic-display
+			jump 10 equal type @large-logic-display
+			jump 12 equal type @switch
+			jump 13 always 0 0
+			draw clear 0 0 0 0 0 0
+			drawflush b
+			control enabled b 0 0 0 0
+			op add i i 1
+			set t @tick
+			jump 15 equal t @tick
+			jump 4 notEqual i @links
+			""";
+
 	public static final String internalName = "triangles";
 	public static final String pixmapCheck = internalName+"-pixmap-load-fail";
 
@@ -33,7 +61,7 @@ public class PicToTri extends Mod {
 	@Override
 	public void init() {
 		Log.info("@ running version: @, with @ threads", internalName,  Vars.mods.getMod(internalName).meta.version, OS.cores);
-
+		if (!Core.settings.getBoolOnce(setting("first-run"))) addDefaultSchematics();
 		Vars.ui.settings.addCategory(bundle("mod-name"), t -> {
 			t.checkPref(setting("debug-mode"), false, changed -> debugMode = changed);
 			t.pref(new SeedSetting(setting("default-seed")));
@@ -58,6 +86,26 @@ public class PicToTri extends Mod {
 		return internalName + "." + name;
 	}
 
+	public void addDefaultSchematics() {
+		Log.info("First run, adding default schems");
+		Schematic.Stile button = new Schematic.Stile(Blocks.switchBlock, 0, 0 , null, (byte) 0);
+		Block[] processors = {Blocks.microProcessor, Blocks.logicProcessor, Blocks.hyperProcessor};
+		LogicBlock.LogicBuild logicBuild = (LogicBlock.LogicBuild) Blocks.microProcessor.newBuilding();
+		logicBuild.updateCode(reloadScript);
+		logicBuild.tile = new Tile(0, 0);
+		for (Block processor: processors) {
+			int offset = -processor.sizeOffset;
+			logicBuild.links.clear();
+			logicBuild.tile = new Tile(1+offset, offset);
+			logicBuild.links.add(new LogicBlock.LogicLink(0, 0, "switch1", true));
+			Schematic.Stile procTile = new Schematic.Stile(processor, 1+offset, offset,
+					logicBuild.config(), (byte) 0);
+			Schematic schem = new Schematic(Seq.with(button, procTile), StringMap.of("name", "Display loader"),
+					1+processor.size, processor.size);
+			schem.labels.add(Core.bundle.get(setting("mod-name")));
+			Vars.schematics.add(schem);
+		}
+	}
 }
 
 class SeedSetting extends SettingsMenuDialog.SettingsTable.Setting {
