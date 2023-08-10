bce658b0a899dd8aba45cd0e831d885b5116fa3d
Mars <49356012+7003Mars@users.noreply.github.com>
2023-08-10 14:24:27 +0800

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,19 @@ version = "1.0"
 with(java) {
     targetCompatibility = JavaVersion.VERSION_1_8
     sourceCompatibility = JavaVersion.VERSION_16
-
 }
 
-sourceSets.main.configure {
-    java.srcDirs("src")
+sourceSets {
+    main {
+        java {
+            srcDirs("src")
+        }
+    }
+    test {
+        java {
+            srcDirs("test")
+        }
+    }
 }
 
 repositories {
@@ -23,6 +31,8 @@ repositories {
 val mindustryVersion by extra("v145.1")
 val jabelVersion by extra("93fde537c7")
 val sdkRoot: String? by extra(System.getenv("ANDROID_HOME") ?: System.getenv("ANDROID_SDK_ROOT"))
+
+val archivesName = base.archivesName.get()
 
 allprojects {
     tasks.withType<JavaCompile> {
@@ -42,6 +52,10 @@ dependencies {
     compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
     compileOnly("com.github.anuken.mindustry:core:$mindustryVersion")
     annotationProcessor("com.github.Anuken:jabel:$jabelVersion")
+
+    testImplementation("com.github.Anuken.Arc:arc-core:$mindustryVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.register("jarAndroid") {
@@ -58,14 +72,13 @@ tasks.register("jarAndroid") {
         val dependencies = (configurations.compileClasspath.get().toList() + configurations.runtimeClasspath.get().toList() + arrayOf(File(platformRoot, "android.jar")).map{ "--classpath $it.path" }).joinToString(" ")
 //      dex and desugar files - this requires d8 in your PATH
         exec {
-            commandLine("d8 $dependencies --min-api 14 --output ${base.archivesName}Android.jar ${base.archivesName}Desktop.jar")
+            commandLine("d8 $dependencies --min-api 14 --output ${archivesName}Android.jar ${archivesName}Desktop.jar")
             workingDir = File("$buildDir/libs")
         }
     }
 }
 
 tasks.named<Jar>("jar") {
-//    val archiveFileName = "${base.archivesName}Desktop.jar"
     val buildVer: String = project.findProperty("modVer") as String? ?: "build-${LocalTime.now()}"
 
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it)})
@@ -79,8 +92,12 @@ tasks.named<Jar>("jar") {
 
 tasks.register<Jar>("deploy") {
     dependsOn("jar", "jarAndroid")
-    from(zipTree("$buildDir/libs/${base.archivesName}Desktop.jar"), zipTree("$buildDir/libs/${base.archivesName}Android.jar"))
+    from(zipTree("$buildDir/libs/${archivesName}Desktop.jar"), zipTree("$buildDir/libs/${archivesName}Android.jar"))
     doLast {
-        delete("$buildDir/libs/${base.archivesName}Desktop.jar", "$buildDir/libs/${base.archivesName}Android.jar")
+        delete("$buildDir/libs/${archivesName}Desktop.jar", "$buildDir/libs/${archivesName}Android.jar")
     }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
