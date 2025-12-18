ba675d15616ec6b4eb5dcca1ef0f39462d5c8487
Mars <49356012+7003Mars@users.noreply.github.com>
2025-12-18 12:37:34 +0800

    Coord checks for shapes added to CodeBuilder

--- src/me/mars/triangles/layout/CodeBuilder.java
+++ src/me/mars/triangles/layout/CodeBuilder.java
@@ -1,14 +1,17 @@
 package me.mars.triangles.layout;
 
 import arc.util.Log;
+import me.mars.triangles.PicToTri;
 import me.mars.triangles.shapes.Shape;
+import me.mars.triangles.shapes.Triangle;
 import mindustry.logic.LExecutor;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodeBuilder {
-    Pattern pattern = Pattern.compile("\\$(-?\\d+)?");
+    public static int MAX_SAFE_COORDINATE = 0b0111111111;
+    static Pattern pattern = Pattern.compile("\\$(-?\\d+)?");
 
     StringBuilder builder = new StringBuilder();
     int lineCount = 0;
@@ -30,7 +33,19 @@ public class CodeBuilder {
         }
     }
 
+    private static boolean checkSafeCoordinate(int coord) {
+        return Math.abs(coord) <= MAX_SAFE_COORDINATE;
+    }
+
     public void appendShape(Shape shape) {
+        if (PicToTri.debugMode) {
+            if (shape instanceof Triangle tri) {
+                if (!(checkSafeCoordinate(tri.x1) && checkSafeCoordinate(tri.x2) && checkSafeCoordinate(tri.x3) &&
+                        checkSafeCoordinate(tri.y1) && checkSafeCoordinate(tri.y2) && checkSafeCoordinate(tri.y3))) {
+                    Log.warn("Triangle contains coords which will overflow: @", tri);
+                }
+            }
+        }
         builder.append(shape.toInstr());
         lineCount += 2;
     }
